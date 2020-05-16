ed8418ef16ed144b98dc9624b9131457d913e50c
Kaunov Nikita Anatolyevich <nkaunov@ozon.ru>
2020-05-16 17:59:10 +0300

    Initial commit

---
+++ lingual_leo.py
@@ -0,0 +1,21 @@
+import random
+
+amount_of_words = input("Как много слов вы хотите занести в словарь")
+aow = int(amount_of_words)
+slovar = {}
+
+for i in range(aow):
+    key = input("Введите слово на английском \n:")
+    value = input("Введите слово на русском \n:")
+    slovar[key] = value
+
+
+for key in slovar.keys():
+    word = list(slovar.keys())
+    key = random.choice(word)
+    print("Переведи слово", key, ": ")
+    answer = input("Ваш вариант перевода\n:")
+    if slovar[key] == answer:
+        print("Все верно! Лев сыт!")
+    else:
+        print("А правильный овет был", slovar[key])
---
+++ multiplication_table.py
@@ -0,0 +1,5 @@
+
+for i in range(1, 10):
+    for k in range(1,10):
+        print(k, "X",i , "=", i*k ,end='\t')
+    print('\t')
